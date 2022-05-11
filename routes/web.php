1457b4531a5b21424427e863762eb2cf622d50cd
mahdikiani <mahdikiani1379@gmail.com>
2022-05-11 11:53:57 +0430

    change in route

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,12 @@ use Illuminate\Support\Facades\Auth;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    //return view('welcome');
+    return view('index');
+});
+Route::get('/about', function () {
+    //return view('welcome');
+    return view('about');
 });
 
 Route::middleware(['guest'])->group(function (){
