11246c08d8b6e07cc76583f8a233a8b462b75c51
mahdikiani <mahdikiani1379@gmail.com>
2022-07-02 18:41:49 +0430

    add methods for addgym , editgym , show gyms

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -2,11 +2,16 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\Gym;
+use App\Models\Gymsimage;
 use App\Models\User;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Container\Container;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\File;
 
 class AdminController extends Container
 {
@@ -94,7 +99,342 @@ class AdminController extends Container
             return redirect('/admin-dashboard/edit-password')->with('danger','رمز عبور فعلی صحیح نبود');
          }
     }
+    public function IndexGyms()
+    {
+        return view('AdminGyms');
+    }
+    public function ShowGyms()
+    {
+        $gyms = Gym::orderBy('id','DESC')->get();
 
+        return view('AdminGyms',compact('gyms'));
+    }
+    public function IndexAddGym()
+    {
+        return view('AdminAddGym');
+    }
+    public function AddGym(Request $request)
+    {
+        
+        $valid = $request->validate([
+            'name'    => 'required|unique:gyms,phone_number',
+            'address'     => 'required|min:10',
+            'phone_number'     => 'required|digits:11|unique:gyms,phone_number',
+            'GenderList'=>'',
+            'football'=>'',
+            'volleyball'=>'',
+            'basketball'=>'',
+            'handball'=>'',
+            'locker_room'=>'',
+            'drinking_water'=>'',
+            'bathroom'=>'',
+            'image1'=>'mimes:jpg,png,jpeg',
+            'image2'=>'mimes:jpg,png,jpeg|max:5048',
+            'image3'=>'mimes:jpg,png,jpeg|max:5048'
+        ]);
+
+       $image1 = time().'-'.$valid['name'].'.'.$request->file('image1')->guessClientExtension();
+       
+       if (isset($valid['image2']) && !empty($valid['image2'])) {
+        $image2 = time().'-'.$valid['name'].'2'.'.'.$request->file('image2')->guessClientExtension();
+
+       }if (isset($valid['image3']) && !empty($valid['image3'])) {
+        $image3 = time().'-'.$valid['name'].'3'.'.'.$request->file('image3')->guessClientExtension();
+
+       }
+        
+        if (isset($valid['football'])) {
+            $valid['football']='1';
+        }
+        else{
+            $valid['football']='0';
+        }
+        if (isset($valid['volleyball'])) {
+            $valid['volleyball']='1';
+        }
+        else{
+            $valid['volleyball']='0';
+        }
+        if (isset($valid['basketball'])) {
+            $valid['basketball']='1';
+        }
+        else{
+            $valid['basketball']='0';
+        }
+        if (isset($valid['handball'])) {
+            $valid['handball']='1';
+        }
+        else{
+            $valid['handball']='0';
+        }
+        if (isset($valid['locker_room'])) {
+            $valid['locker_room']='1';
+        }
+        else{
+            $valid['locker_room']='0';
+        }
+        if (isset($valid['drinking_water'])) {
+            $valid['drinking_water']='1';
+        }
+        else{
+            $valid['drinking_water']='0';
+        }
+        if (isset($valid['bathroom'])) {
+            $valid['bathroom']='1';
+        }
+        else{
+            $valid['bathroom']='0';
+        }
+        
+        $gym = Gym::create([
+            'name'    => $valid['name'],
+            'address'     => $valid['address'],
+            'phone_number'     => $valid['phone_number'],
+            'gender'=>$valid['GenderList'],
+            'football'=>$valid['football'],
+            'volleyball'=>$valid['volleyball'],
+            'basketball'=>$valid['basketball'],
+            'handball'=>$valid['handball'],
+            'locker_room'=>$valid['locker_room'],
+            'drinking_water'=>$valid['drinking_water'],
+            'bathroom'=>$valid['bathroom']
+        ]);
+        if ($gym==true) {
+            
+            if (isset($image1) && !empty($image1)) {
+                
+                $save = $request->image1->move(public_path('images'),$image1);
+                
+                $img = Gymsimage::create([
+                    'gyms_id'=>$gym->id,
+                    'src'=>$image1  
+                ]);
+            }
+            if (isset($image2) && !empty($image2)) {
+                $save = $request->image2->move(public_path('images'),$image2);
+                $img = Gymsimage::create([
+                    'gyms_id'=>$gym->id,
+                    'src'=>$image2  
+                ]);
+            }
+            if (isset($image3) && !empty($image3)) {
+                $save = $request->image3->move(public_path('images'),$image3);
+                $img = Gymsimage::create([
+                    'gyms_id'=>$gym->id,
+                    'src'=>$image3  
+                ]);
+            }
+        }
+        return redirect('/admin-dashboard/gyms')->with('success','اطلاعات با موفقيت ذخيره شد');
+    }
+    
+    public function IndexEditGym(Gym $gym ,Gymsimage $gymimage, Request $request)
+    {
+        $gym = Gym::find($request->id);
+        $gymimage = Gymsimage::where('gyms_id',$request->id)->get();
+        return view('AdminEditGym',compact('gym','gymimage'));
+    }
+
+    public function EditGym(Request $request,$id)
+    {
+        $gym = Gym::find($id);
+
+        $gymimage = Gymsimage::where('gyms_id',$id)->get();
+        $gymimage->toArray();
+        
+        
+        //dd($destination);
+        $valid = $request->validate([
+            'name'    => 'required',
+            'address'     => 'required|min:10',
+            'phone_number'     => ['required',
+                                 'digits:11',Rule::unique('gyms','phone_number')->ignore($id)],
+            'GenderList'=>'',
+            'football'=>'',
+            'volleyball'=>'',
+            'basketball'=>'',
+            'handball'=>'',
+            'locker_room'=>'',
+            'drinking_water'=>'',
+            'bathroom'=>'',
+            'image1'=>'mimes:jpg,png,jpeg',
+            'image2'=>'mimes:jpg,png,jpeg|max:5048',
+            'image3'=>'mimes:jpg,png,jpeg|max:5048'
+        ]);
+        if (isset($valid['image1']) && !empty($valid['image1'])) {
+       $image1 = time().'-'.$valid['name'].'.'.$request->file('image1')->guessClientExtension();
+        }
+       if (isset($valid['image2']) && !empty($valid['image2'])) {
+        $image2 = time().'-'.$valid['name'].'2'.'.'.$request->file('image2')->guessClientExtension();
+
+       }if (isset($valid['image3']) && !empty($valid['image3'])) {
+        $image3 = time().'-'.$valid['name'].'3'.'.'.$request->file('image3')->guessClientExtension();
+
+       }
+        
+        if (isset($valid['football'])) {
+            $valid['football']='1';
+        }
+        else{
+            $valid['football']='0';
+        }
+        if (isset($valid['volleyball'])) {
+            $valid['volleyball']='1';
+        }
+        else{
+            $valid['volleyball']='0';
+        }
+        if (isset($valid['basketball'])) {
+            $valid['basketball']='1';
+        }
+        else{
+            $valid['basketball']='0';
+        }
+        if (isset($valid['handball'])) {
+            $valid['handball']='1';
+        }
+        else{
+            $valid['handball']='0';
+        }
+        if (isset($valid['locker_room'])) {
+            $valid['locker_room']='1';
+        }
+        else{
+            $valid['locker_room']='0';
+        }
+        if (isset($valid['drinking_water'])) {
+            $valid['drinking_water']='1';
+        }
+        else{
+            $valid['drinking_water']='0';
+        }
+        if (isset($valid['bathroom'])) {
+            $valid['bathroom']='1';
+        }
+        else{
+            $valid['bathroom']='0';
+        }
+
+        $gym->name = $valid['name'];
+        $gym->address = $valid['address'];
+        $gym->phone_number = $valid['phone_number'];
+        $gym->Gender = $valid['GenderList'];
+        $gym->football = $valid['football'];
+        $gym->volleyball = $valid['volleyball'];
+        $gym->basketball = $valid['basketball'];
+        $gym->handball = $valid['handball'];
+        $gym->locker_room = $valid['locker_room'];
+        $gym->drinking_water = $valid['drinking_water'];
+        $gym->bathroom = $valid['bathroom'];
+
+        $gym->save();
+        // if ($gym==true) {
+        //     return redirect('/admin-dashboard/gyms')->with('success','اطلاعات با موفقيت ویرایش شد');
+        // }
+        // else{
+        //     return redirect('/admin-dashboard/gyms')->with('danger','خطا در ویرایش اطلاعات');
+        // }
+        if ($gym==true) {
+            
+            if (isset($image1) && !empty($image1)) {
+                if (!empty($gymimage[0])) {
+                    $destination = 'images/'.$gymimage[0]['src'];
+                    if(File::exists($destination)){
+                    File::delete($destination);
+                    }
+                    $save = $request->image1->move(public_path('images'),$image1);
+                    Gymsimage::where('id', $gymimage[0]['id'])->update(['src' => $image1]);
+                }
+                else {
+                    $save = $request->image1->move(public_path('images'),$image1);
+                    $img = Gymsimage::create([
+                    'gyms_id'=>$gym->id,
+                    'src'=>$image1  
+                ]);
+                }
+
+                
+                // $img = Gymsimage::create([
+                //     'gyms_id'=>$gym->id,
+                //     'src'=>$image1  
+                // ]);
+            }
+            if (isset($image2) && !empty($image2)) {
+                if (!empty($gymimage[1])) {
+                    $destination = 'images/'.$gymimage[1]['src'];
+                    if(File::exists($destination)){
+                    File::delete($destination);
+                    }
+                    $save = $request->image2->move(public_path('images'),$image2);
+                    Gymsimage::where('id', $gymimage[1]['id'])->update(['src' => $image2]);
+                }
+                else {
+                    $save = $request->image2->move(public_path('images'),$image2);
+                    $img = Gymsimage::create([
+                    'gyms_id'=>$gym->id,
+                    'src'=>$image2  
+                ]);
+                }
+            }
+            if (isset($image3) && !empty($image3)) {
+                if (!empty($gymimage[2])) {
+                    $destination = 'images/'.$gymimage[2]['src'];
+                    if(File::exists($destination)){
+                    File::delete($destination);
+                    }
+                    $save = $request->image3->move(public_path('images'),$image3);
+                    Gymsimage::where('id', $gymimage[2]['id'])->update(['src' => $image3]);
+                }
+                else {
+                    $save = $request->image3->move(public_path('images'),$image3);
+                    $img = Gymsimage::create([
+                    'gyms_id'=>$gym->id,
+                    'src'=>$image3  
+                ]);
+                }
+            }
+        
+        }
+        return redirect('/admin-dashboard/gyms')->with('success','اطلاعات با موفقيت ویرایش شد');
+
+    }
+
+
+    public function IndexDestroyGym(Request $request)
+    {
+        
+        $gym = Gym::find($request->id);
+        $gymimage = Gymsimage::where('gyms_id',$request->id)->get()->toArray();
+       
+
+        for ($i=0; $i < count($gymimage) ; $i++) { 
+            $destination = 'images/'.$gymimage[$i]['src'];
+                if(File::exists($destination)){
+                    
+                    File::delete($destination);
+                    
+                }
+        }
+        $status = $gym->delete();
+        
+        if($status == true)
+            return redirect('/admin-dashboard/gyms')->with('danger','حذف انجام شد');
+            else
+            return redirect('/admin-dashboard/gyms')->with('danger','حذف انجام نشد');
+    }
+    public function ImageDelete(Gymsimage $gymimage = null,Request $request)
+    {
+        $gymimage = Gymsimage::find($request->id);
+        
+        $destination = 'images/'.$gymimage->src;
+                if(File::exists($destination)){
+                    
+                    File::delete($destination);
+                }
+        $gymimage->delete();
+        return redirect()->back();
+
+    }
     /**
      * Show the form for creating a new resource.
      *
@@ -133,11 +473,6 @@ class AdminController extends Container
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
-    {
-        //
-    }
-
     /**
      * Update the specified resource in storage.
      *
@@ -147,7 +482,7 @@ class AdminController extends Container
      */
     public function update(Request $request, $id)
     {
-        //
+        
     }
 
     /**
